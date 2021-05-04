22df5e1c1527c91a3b40f769b37b3ce0f3076e3e
Claudio Alves Jr <claudio.alves1985@hotmail.com>
2021-05-04 12:27:57 -0300

    nao foi usado o arquivo JS, só comentei ele todo

--- desafio1/script.js
+++ desafio1/script.js
@@ -1,5 +1,6 @@
-let imagem = document.getElementById('logo')
+/*let imagem = document.getElementById('logo')
 
 function zoom(){
     imagem.style.height = '200%'
-}
+}*/
+//nao usei o codigo mas deixei ai
